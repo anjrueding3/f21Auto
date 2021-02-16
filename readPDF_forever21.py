7aa789ba54f255af70c4b4fb96ce28c97753b08d
Andrew Ding <anjrued@gmail.com>
2021-02-16 12:09:15 -0800

    formatted

--- readPDF_forever21.py
+++ readPDF_forever21.py
@@ -7,15 +7,13 @@ def extractPDF_Text(fileName):
     firstPage = pdf.getPage(0)
     string = firstPage.extractText()
     return string
- 
- 
+    
 #find In house date for forever 21 PO's, returns ihd as string
 def findIHD(pdf_text):
     indexOrder = pdf_text.index('(IHD)')
     ihdIndex = indexOrder + 5
     ihd = pdf_text[ihdIndex:ihdIndex + 10]
     return ihd
-
 
 #find PO number, returns first string in digitString list aka the actual PO number
 def findPO(pdf_text):
@@ -24,19 +22,18 @@ def findPO(pdf_text):
     return digitStrings[0]
 
 
+
 #parse text file for PO, return PO
 def extractPO(fileName):
     pdfText = extractPDF_Text(fileName)
     PO = findPO(pdfText)
     return PO
 
-
 #parse text file for IHD, return ihd
 def extractIHD(fileName):
     pdfText = extractPDF_Text(fileName)
     IHD = findIHD(pdfText)
     return IHD
-
 
 #pass in directory, return dictionary with po and ihd key-value pairs
 def extractPoIHD_Dictionary(directory):
@@ -48,14 +45,16 @@ def extractPoIHD_Dictionary(directory):
 
     return parsedData
 
-
 #print formatted dictionary
 def printPrettyDictionary(dictionary):
     for key in dictionary:
         print(key + ': ' + dictionary[key])
         
+
+
     
 #os.path.join(directory, directoryList[i]) = directory + file name
+#need to think of potential errors. If mommy adds something wrong to folder.
     
 #for i in range(len(directoryList)):
    # parsedData = parsePoIHD(os.path.join(directory, directoryList[i]))
