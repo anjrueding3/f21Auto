6029af66ae9a4650985069ecde1050e2a373e05f
Andrew Ding <anjrued@gmail.com>
2021-02-17 23:32:47 -0800

    created ExcelData object

--- src/ExcelData.py
+++ src/ExcelData.py
@@ -25,14 +25,6 @@ class ExcelData:
         print('PO#: ' + str(self.PO) + '\nIHD: ' + self.IHD + '\nStyle Number: ' + self.styleNum) 
 
 
-data = ExcelData('6/15/20', 20000000, 'UN-JFLKDJLK')
-data.getInfo()
-
-data.setIHD('06/20/14')
-
-data.getInfo()
-
-
 
 
 
